3bbb788e40003891adcfab7400f37a31d0aec329
gusta <gustavoschramm.sgg@gmail.com>
2024-09-01 20:33:01 -0300

    update verification
    update in the verification of the CPF and the creation of the email

--- src/services/auth/auth-service.js
+++ src/services/auth/auth-service.js
@@ -12,23 +12,32 @@ export class AuthService {
 
   async signUser(userData) {
       try {
+
+          let transform = userData.cpf
+          transform = transform.replace(/[^\d]+/g, '');
+          userData.cpf = transform
+
+          //Fazer uma verificação por email 
+          const existingUseremail = await this.getUserByEmail(userData.email);
+          if (existingUseremail) {
+              throw new Error("Usuário já existe Email");
+          }
           
+          // Verificação de existência do usuário no DB
+          const existingUserCpf = await this.getUserByCPF(userData.cpf);
+          if (existingUserCpf) {
+              throw new Error("Usuário já existe");
+          }
+
           JSON.stringify(userData)
           console.log(userData);
           console.log(userData.cpf)
+
           // Validação do CPF
           if (!CPFUtils.validarCPF(userData.cpf)) {
               throw new Error("CPF inválido");
           }
           console.log("cpf valido");
-          
-          // Verificação de existência do usuário no DB
-          const existingUser = await this.getUserByCPF(userData.cpf);
-          if (existingUser) {
-              throw new Error("Usuário já existe");
-          }
-
-          console.log("talvez");
           
 
           // Leitura do banco de dados
@@ -48,42 +57,60 @@ export class AuthService {
       }
   }
 
-// Obtém um usuário pelo CPF
-async getUserByCPF(cpf) {
-    try {
-        const data = await fs.promises.readFile(dbPath, { encoding: 'utf8' });
-        const users = JSON.parse(data).users;
-        return users.find(user => user.cpf === cpf) || null;
-    } catch (error) {
-        console.log(error.message);
-        throw error;
-    }
-}
+  // Obtain a user by Cpf
+  // Repair this verification-----
+  async getUserByCPF(cpf) {
+        try {
+          // Padronize o CPF ao comparar
+          cpf = cpf.replace(/[^\d]+/g, '');
 
-/*//input user in database
-  async signUser(userData){
-    
+          const data = await fs.promises.readFile(dbPath, { encoding: 'utf8' });
+          const users = JSON.parse(data).users;
+            
+          return users.find(user => user.cpf === cpf) || null;
+        } catch (error) {
+          console.log(error.message);
+          throw error;
+      }
+  }
+
+  // Obtain a user by Email
+  async getUserByEmail(email) {
     try {
-      //find mock database
-      const data = (await fs.promises.readFile(dbPath,{encoding:'utf8'}))
-      //parsing string to object
-      let res = JSON.parse(data)
-      // transform userData from string to object 
-      let newUser = JSON.parse(userData)
-      //push user to array
-      res.users.push(newUser)
-   
-      // transform the array in string again
-      res = JSON.stringify(res)
-      //write in database
-      await fs.promises.writeFile(dbPath,res,{
-        encoding:'utf-8'
-      })
-      return 'usuário cadastrado com sucesso'
-    } catch (error) { 
-      console.log(error.message)
+      const data = await fs.promises.readFile(dbPath, { encoding: 'utf8' });
+      const users = JSON.parse(data).users;
+
+      return users.find(user => user.email === email) || null;
+    } catch (error) {
+      console.log(error.message);
+      throw error;
     }
-  }*/
+  }
+
+  /*//input user in database
+    async signUser(userData){
+    
+      try {
+        //find mock database
+        const data = (await fs.promises.readFile(dbPath,{encoding:'utf8'}))
+        //parsing string to object
+        let res = JSON.parse(data)
+        // transform userData from string to object 
+        let newUser = JSON.parse(userData)
+        //push user to array
+        res.users.push(newUser)
+   
+        // transform the array in string again
+        res = JSON.stringify(res)
+        //write in database
+        await fs.promises.writeFile(dbPath,res,{
+        encoding:'utf-8'
+        })
+        return 'usuário cadastrado com sucesso'
+      } catch (error) { 
+      console.log(error.message)
+      }
+    }*/
 
 }
 
